049efdb4709dc3a89ba6af034bb10ede18be6653
zovalger <zovalger@gmail.com>
2023-07-27 12:38:54 -0400

    fix: product task

--- src/sockets/index.ts
+++ src/sockets/index.ts
@@ -1,9 +1,11 @@
 import { Server } from "socket.io";
-import StopwatchSocket from "./StopWatch.socket";
+import StopwatchSocket from "./Stopwatch.socket";
+import SocketEventsSystem from "../config/SocketEventsSystem";
 
 const initSockets = async (io: Server) => {
 	io.on("connection", (socket) => {
 		StopwatchSocket(io, socket);
+		socket.on(SocketEventsSystem.disconnect, () => {});
 	});
 };
 
--- src/types.ts
+++ src/types.ts
@@ -1,5 +1,4 @@
 import mongoose, { Document } from "mongoose";
-import { StopwatchEvents } from "./config/SocketEventsSystem";
 
 // ****************************************************************************
 // 										              Productos
