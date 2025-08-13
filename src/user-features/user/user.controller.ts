0e5206032461cce0f6c33c59dce3b06e356a7253
zovalger <zovalger@gmail.com>
2025-08-12 22:44:20 -0400

    fix: user by id response

--- src/user-features/user/user.controller.ts
+++ src/user-features/user/user.controller.ts
@@ -32,8 +32,10 @@ export class UserController {
   // }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.userService.findOne(id);
+  async findOne(@Param('id') id: string) {
+    const data = await this.userService.findOne(id);
+
+    return { data };
   }
 
   @Patch()
